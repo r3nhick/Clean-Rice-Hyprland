bab056c316bdf351152b89a25434ef6211728258
r3nhick <userr3nprogram@email.com>
2025-07-20 17:49:29 +0000

    realise v1

---
+++ .bashrc
@@ -0,0 +1,12 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto't
+PS1='[\u@\h \W]\$ '
+
+fastfetch --logo /home/user/Rice/Images/makima_edit.png --logo-type kitty  --logo-width 30
---
+++ packages.sh
@@ -0,0 +1,2 @@
+exec sudo pacman -Syu hyprland hyprlock hyprpaper kitty waybar nemo networkmanager network-manager-applet nano git base-devel rofi ttf-dejavu ttf-liberation ttf-font-awesome ttf-jetbrains-mono-nerd noto-fonts noto-fonts-emoji noto-fonts-cjk sddm fastfetch cava htop neovim firefox obsidian telegram-desktop font-manager code nwg-look 
+
